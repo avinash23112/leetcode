41fd855a448e319ef603f637847e2b779ab1bc86
avinash23112 <143744619+avinash23112@users.noreply.github.com>
2023-09-01 03:20:46 +0530

    Time: 7 ms (84.41%) | Memory: 19.2 MB (6.62%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,53 +1,63 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        vector<int>v;
-        int n=nums.size();
-        // if(n==1)
-        //     return n;
-        for(int i=0;i<n-1;i++){
-            if(nums[i]!=nums[i+1] ){
-                v.push_back(nums[i]);
-            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // //correct
+        // vector<int>v;
+        // int n=nums.size();
+        // // if(n==1)
+        // //     return n;
+        // for(int i=0;i<n-1;i++){
+        //     if(nums[i]!=nums[i+1] ){
+        //         v.push_back(nums[i]);
+        //     }
+        // }
+        // v.push_back(nums[n - 1]);
+        // nums=v;
+        // return v.size();
+
+
+
+
+
+
+
+        set<int>set;
+        for(int i=0;i<nums.size();i++){
+            set.insert(nums[i]);
         }
-        v.push_back(nums[n - 1]);
-        nums=v;
-        return v.size();
+        int i=0;
+        for(auto x:set){
+            nums[i]=x;
+            i++;
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // set<int>set;
-        // for(int i=0;i<nums.size();i++){
-        //     set.insert(nums[i]);
-        // }
-        // int i=0;
-        // for(auto x:set){
-        //     nums[i]=x;
-        //     i++;
-        // }
-
-        // for(int x=0;i<set.size();i++){
-        //     nums[i]=set[x];
-        //     i++;
-        // }
-        //return set.size();
+        
+        return set.size();
     }
 };
