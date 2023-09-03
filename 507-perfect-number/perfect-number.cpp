9be0133c66014754ccd27312d0da3518a91459d6
avinash23112 <143744619+avinash23112@users.noreply.github.com>
2023-09-04 01:18:43 +0530

    Time: 1039 ms (54.10%) | Memory: 6 MB (44.91%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -2,14 +2,32 @@ class Solution {
 public:
     bool checkPerfectNumber(int num) {
         int num1=num;
-        int sum=0;
-        for(int i=1;i<num;i++){
-            if(num1%i==0) sum=sum+i;
+        int sum=1;
+        if(num==1) return 0;
+
+        for(int i=2;i<=(num)/2;i++){
+            if(num1%(i)==0) sum=sum+i;
         }
         if(sum==num)
             return 1;
         else 
             return 0;
+
+
+        // int s=1;
+        // if(num==1) return false;
+        // for(int i=2;i<=sqrt(num);i++){
+        //     if(num%i==0){
+        //         if(num/i==i){
+        //           s=s+i;
+        //         }
+        //         else{
+        //             s=s+i+(num/i);
+        //         }
+        //     }
+        // }
+
+        // return s==num;
         
     }
 };
