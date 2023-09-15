e4f5529dc0154ee877cff6782492d55b12205857
avinash23112 <143744619+avinash23112@users.noreply.github.com>
2023-09-16 01:53:56 +0530

    Time: 55 ms (84.90%) | Memory: 39.3 MB (30.76%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.cpp
+++ 2486-most-frequent-even-element/most-frequent-even-element.cpp
@@ -2,39 +2,21 @@ class Solution {
 public:
     int mostFrequentEven(vector<int>& nums) {
 
-        map<int,int>map;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%2==0)
-                map[nums[i]]++;
+        map<int,int> mp;
+        for( int i : nums) {
+            if(i%2 == 0) {
+                mp[i]++;
+            }
         }
-        
-        int maxfre=INT_MIN;
-        int maxnum;
-        int smallmaxfre,smallmaxnum;
-        
-        for(auto i:map){
-            if(i.first%2==0){
-                
-                if(i.second >= maxfre ){
-                    if(i.second!=maxfre){
-                        smallmaxfre=i.second;
-                        smallmaxnum = i.first;
-                    }
-                    maxfre = i.second;
-                    maxnum = i.first;
-                    cout<<maxnum<<" "<<maxfre<<endl; //print
-                }
-                
+
+        int ans = -1, max = 0;
+        for( auto i: mp) {
+            if(i.second > max) {
+                max = i.second;
+                ans = i.first;
             }
             
         }
-        cout<<maxnum;//print
-        
-        if (smallmaxfre==maxfre)
-            return smallmaxnum;
-        if (maxfre>smallmaxfre)
-            return maxnum;
-        
-        return -1;
+        return ans;
     }
 };
